9c963ae08439ecb4e112d315f4a0f26f47d2cb12
Patricia Manalang <patriciamanalang06@gmail.com>
2022-11-01 22:00:24 +0000

    Verified that new entries are saved to local storage along with the rest of the data model on page reload.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var $photoUrl = document.querySelector('#photo-url');
+var $photoUrl = document.querySelector('#photourl');
 var $img = document.querySelector('.img');
 var $newEntryForm = document.querySelector('#new-entry-form');
 
@@ -13,9 +13,12 @@ function handleSubmit(event) {
   event.preventDefault();
   var dataEntry = {};
   dataEntry.title = $newEntryForm.elements.title.value;
-  dataEntry.image = $newEntryForm.elements.image.value;
+  dataEntry.url = $newEntryForm.elements.photourl.value;
   dataEntry.notes = $newEntryForm.elements.notes.value;
+  dataEntry.entryId = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.unshift(dataEntry);
+  $img.setAttribute('src', 'images/placeholder-image-square.jpg');
   $newEntryForm.reset();
 }
-
 $newEntryForm.addEventListener('submit', handleSubmit);
