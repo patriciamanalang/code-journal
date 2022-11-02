4cdfc1067f83a27c029f9f4597bf986aea85ced3
Patricia Manalang <patriciamanalang06@gmail.com>
2022-11-02 19:27:12 +0000

    Defined a function that takes a single journal entry object and returns a DOM tree that matches one of the example entries in the HTML.

--- js/main.js
+++ js/main.js
@@ -22,3 +22,32 @@ function handleSubmit(event) {
   $newEntryForm.reset();
 }
 $newEntryForm.addEventListener('submit', handleSubmit);
+
+// function renderJournalEntry(entry) {
+//   var $li = document.createElement('li');
+//   var $rowDiv = document.createElement('div');
+//   $rowDiv.setAttribute('class', 'row');
+//   $li.appendChild($rowDiv);
+//   var $columnHalfDiv = document.createElement('div');
+//   $columnHalfDiv.setAttribute('class', 'column-half');
+//   $rowDiv.appendChild($columnHalfDiv);
+//   var $imgDiv = document.createElement('div');
+//   $columnHalfDiv.appendChild($imgDiv);
+//   var $img = document.createElement('img');
+//   $img.setAttribute('class', 'entry-image');
+//   $img.src = entry.url;
+//   $imgDiv.appendChild($img);
+//   var $secondColumnHalfDiv = document.createElement('div');
+//   $secondColumnHalfDiv.setAttribute('class', 'column-half');
+//   $rowDiv.appendChild($secondColumnHalfDiv);
+//   var $entryImageTextDiv = document.createElement('div');
+//   $entryImageTextDiv.setAttribute('class', 'entry-image-text');
+//   $secondColumnHalfDiv.appendChild($entryImageTextDiv);
+//   var $h2 = document.createElement('h2');
+//   $h2.textContent = entry.title;
+//   $entryImageTextDiv.appendChild($h2);
+//   var $p = document.createElement('p');
+//   $p.textContent = entry.notes;
+//   $entryImageTextDiv.appendChild($p);
+//   return $li;
+// }
